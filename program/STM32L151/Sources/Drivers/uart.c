908dbbcb4e7b2c0a35909cfde3cab46c8a622e9c
xiaoxiami <changxiaodong.cxd@gmail.com>
2016-06-06 10:12:07 +0800

    send by XBee

--- program/STM32L151/Sources/Drivers/uart.c
+++ program/STM32L151/Sources/Drivers/uart.c
@@ -25,13 +25,13 @@ void Init_XBEE_UART()
     USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;   
     USART_Init(USART1, &USART_InitStructure);  
-    USART_ITConfig(USART1,USART_IT_RXNE, ENABLE);
+    //USART_ITConfig(USART1,USART_IT_RXNE, ENABLE);
     
-    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
+    //NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    //NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
+    //NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    //NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    //NVIC_Init(&NVIC_InitStructure);
     USART_Cmd(USART1, ENABLE);  
 
 }
